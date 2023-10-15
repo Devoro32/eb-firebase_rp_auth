f46ef29d7074b5be4c2c1c04d6ddae5b395287c3
Edane Barton <edanebarton@gmail.com>
2023-10-15 13:25:23 +0100

    baseline

---
+++ android/app/src/main/kotlin/com/example/fp_rp_auth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fp_rp_auth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/export.dart
@@ -0,0 +1,7 @@
+//core
+export 'package:flutter/material.dart';
+//export 'package:flutter/foundation.dart';
+export 'dart:convert';
+export 'package:hooks_riverpod/hooks_riverpod.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:fp_rp_auth/firebase_options.dart';
